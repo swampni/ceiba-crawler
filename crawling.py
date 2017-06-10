a5244c6b7d82daaa4cab60f3f99df366e3ac24fc
Splend1d <a24998667@gmail.com>
2017-06-10 20:58:55 +0800

    added try except
    to prevent some digital dummies who think that 11-1 is a number making the program crash

--- crawling.py
+++ crawling.py
@@ -102,8 +102,11 @@ class Crawler():
         else:
             for i in range(1, entites):
                 
-                week =  int(self.browser.find_element_by_xpath(
-                '//body/div/div/div/div/table/tbody/tr['+str(i+1)+']/td[1]').text[1:-1])
+                try:
+                    week =  int(self.browser.find_element_by_xpath(
+                    '//body/div/div/div/div/table/tbody/tr['+str(i+1)+']/td[1]').text[1:-1])
+                except:
+                    continue
                 description = ''
                 description  +=  self.browser.find_element_by_xpath(
                 '//body/div/div/div/div/table/tbody/tr['+str(i+1)+']/td[3]').text+'\n'
